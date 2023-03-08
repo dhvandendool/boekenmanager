593d866b302614b682cb89705b03553a71d2c1e0
bitmaze <frederik@bitmaze.nl>
2023-03-08 20:39:17 +0100

    added jest typings and removed karma

---
+++ setup-jest.ts
@@ -0,0 +1,10 @@
+import 'jest-preset-angular/setup-jest';
+
+declare var global: any;
+
+const style = global['document'].createElement('style');
+style.innerHTML = `
+  .mat-theme-loaded-marker {
+    display: none;
+}`;
+global['document'].head.appendChild(style);
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideMockStore } from '@ngrx/store/testing';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports: [
+        MatToolbarModule,
+        RouterModule
+      ],
+      providers: [
+        provideMockStore({})
       ],
     }).compileComponents();
   });
@@ -16,16 +26,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fancom'`, () => {
+  it(`should have as title 'Boeken manager'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fancom');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fancom app is running!');
+    expect(app.title).toEqual('Boeken manager');
   });
 });
