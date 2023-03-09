eb8ac4fb3267402bc9a5a3a793b058bb4cc7183d
bitmaze <frederik@bitmaze.nl>
2023-03-09 20:19:46 +0100

    added comment on setup-jest angular material fix

--- setup-jest.ts
+++ setup-jest.ts
@@ -3,6 +3,7 @@ import 'jest-preset-angular/setup-jest';
 declare var global: any;
 
 const style = global['document'].createElement('style');
+// Fixes angular material warning (see: https://github.com/thymikee/jest-preset-angular/issues/83)
 style.innerHTML = `
   .mat-theme-loaded-marker {
     display: none;
